a8b9c3c10fd0404d2ec0933402b1d1510871a011
Gershon Dublon <g@ger.sh>
2021-05-21 22:39:08 +0200

    scripts for recording NOAA weather satellite signals from an RTL-SDR

---
+++ groundstation.py
@@ -0,0 +1,187 @@
+import predict
+import datetime
+import operator
+import time
+import threading
+import logging
+import cfg
+import os
+import sys
+import sox
+import boto3
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+
+# groundstation configuration 
+configFile = 'groundstation.cfg'
+
+# if no config file is given, use the one above
+if len(sys.argv) > 2:
+    logging.warning("Usage: {} [groundstation.cfg]".format(sys.argv[0]))
+    exit(-1)
+elif len(sys.argv) == 2:
+    configFile = sys.argv[1]
+
+# load config file
+config = cfg.get(configFile)
+
+# TLE file should be updated on a regular basis by a separate process
+tlePath = os.path.join(config.get('DIRS', 'tleDir'), config.get('DIRS', 'tleFile'))
+
+# QTH (ground location)
+qth = (float(config.get('QTH','lat')), float(config.get('QTH','lon')), float(config.get('QTH','alt')))
+
+# minimum elevation (angle in degrees) considered for recording
+minElev = float(config.get('QTH', 'minElev'))
+
+# global AWS S3 object
+s3 = boto3.resource('s3')
+
+# a handy place to keep state about the satellites beingg recording
+class WeatherSatellite:
+    def __init__(self, satID, frequency):
+        self.identifier = satID
+        self.frequency = frequency
+        self.TLE = None
+        self.nextPass = None
+    
+    def predictNextPass(self, qth):
+        p = predict.transits(self.TLE, qth)
+        transit = next(p)
+        while(transit.peak()['elevation'] < minElev):
+            transit = next(p)
+        dt_ts = datetime.datetime.fromtimestamp(transit.start, tz=datetime.timezone.utc)
+        self.nextPass = SatPass(dt_ts,  transit.duration(), transit.peak()['elevation'])
+        return self.nextPass
+
+class SatPass:
+    def __init__(self, passTime, passDuration, passElevation):
+        self.passTime = passTime
+        self.passDuration = passDuration
+        self.passElevation = passElevation
+        self.lastUpdated = datetime.datetime.now(datetime.timezone.utc)
+        
+# update an array of weather sats from a TLE file
+def updateTLE(satellites, tleFilePath):
+    try:
+        logging.info('Ingesting TLEs from {}'.format(tleFilePath))
+        tleFile=open(tleFilePath)
+        tleData=tleFile.readlines()
+        tleFile.close()
+        for sat in satellites:
+            tle=[]
+            for i, line in enumerate(tleData):
+                if(sat.identifier in line): 
+                    for l in tleData[i:i+3]:
+                        tle.append(l.strip('\r\n').rstrip())
+            sat.TLE = tle
+    except OSError as e:
+        logging.warning('OS Error during command: ' + ' '.join(cmdline))
+        logging.warning('OS Error: ' + e.strerror)
+
+
+
+# record demodulated signals over a given duration, breaking the recordings into chunks 
+def recordChunksFM(frequency, totalDuration, chunkDuration):
+    # options for rtl_fm, which captures and demodulates FM signals
+    # rtl_fm is an external application included with rtl-sdr
+    rtl_fm = ['/usr/bin/rtl_fm',
+               '-f', str(frequency),                    # center frequency
+               '-s', config.get('SDR', 'samplerate'),   # sample rate of demodulated signal
+               '-g', config.get('SDR', 'gain'),         # SDR RF gain
+               '-F', '9',                               # enable downsample filter
+               '-E', 'deemp'                            # enable de-emphasis filter
+               '-p', config.get('SDR', 'shift'),        # SDR ppm error
+               '-T']                                    # enable bias tee
+    timeLeft = totalDuration
+    filecount = 0
+    while(timeLeft > 20):
+        outfileName = 'signalchunk_{}'.format(filecount)
+        outfilePath_raw = os.path.join(config.get('DIRS', 'raw'), "{}.raw".format(outfileName))
+        buildShipThread = threading.Thread(target=buildShip, args=(outfileName,))
+        try:
+            logging.info('Starting RF recording and demod (rtl_fm): chunk {}'.format(filecount))
+            child = subprocess.Popen(rtl_fm.append(outfilePath_raw))
+            time.sleep(chunkDuration)
+            child.terminate()
+            filecount = filecount + 1
+            timeLeft = timeLeft - chunkDuration
+            logging.info('Starting transcode/decode/upload thread')
+            buildShipThread.start()
+        except OSError as e:
+            logging.warning('OS Error during command: ' + ' '.join(cmdline))
+            logging.warning('OS Error: ' + e.strerror)
+
+
+# transcode raw recording file, process APT decode, upload to S3, remove files
+# intended to be spun off as a thread while recording continues
+def buildShip(filename):
+    in_raw = os.path.join(config.get('DIRS', 'raw'), '{}.raw'.format(filename))
+    out_wav = os.path.join(config.get('DIRS', 'wav'), '{}.wav'.format(filename))
+    out_mp3 = os.path.join(config.get('DIRS', 'mp3'), '{}.mp3'.format(filename))
+    out_img = os.path.join(config.get('DIRS', 'img'), '{}.png'.format(filename))
+
+    # sox transformer: raw to wav
+    sox_raw2wav = sox.Transformer()
+    sox_raw2wav.set_input_format(file_type='wav',rate=config.get('SDR', 'samplerate'),bits=16,channels=1,encoding='signed-integer')
+    sox_raw2wav.set_output_format(file_type='wav',rate=config.get('SDR', 'wavrate'))
+    logging.info('Starting raw to wav resample (sox)')
+    success = sox_raw2wav.build(in_raw, out_wav)
+    if not success:
+        logging.warning("Raw to wav resample failed!")
+
+    # sox transformer: raw to mp3
+    sox_raw2mp3 = sox.Transformer()
+    sox_raw2mp3.set_input_format(file_type='wav',rate=config.get('SDR', 'samplerate'),bits=16,channels=1,encoding='signed-integer')
+    sox_raw2mp3.set_output_format(file_type='mp3',rate=config.get('SDR', 'mp3rate'))
+    logging.info('Starting raw to mp3 resample/transcode (sox)')
+    success = sox_raw2wav.build(in_raw, out_mp3)
+    if not success:
+        logging.warning("Raw to mp3 resample/transcode failed!")
+
+    # aptdec: decode APT from wav
+    logging.info('Starting APT decode (aptdec)')
+    aptdec = ['./aptdec', out_wav, '-o', out_img]
+    proc = subprocess.Popen(aptdec)
+    proc.wait()
+
+    # upload files to S3
+    logging.info('Starting image chunk upload')
+    img = open(out_img, 'rb')
+    s3.Bucket('ground-station-prototype-eb').put_object(Key='image/{}.png'.format(filename), Body=img)
+    logging.info('Starting audio chunk upload')
+    mp3 = open(out_mp3, 'rb')
+    s3.Bucket('ground-station-prototype-eb').put_object(Key='audio/{}.mp3'.format(filename), Body=mp3)
+
+    # remove files from local 
+    logging.info('Removing local files')
+    os.remove(in_raw)
+    os.remove(out_wav)
+    os.remove(out_mp3)
+    os.remove(out_img)
+    
+
+if __name__ == "__main__":
+    satIDs = config.getlist('SATELLITES', 'identifiers')
+    frequencies = config.getlist('SATELLITES', 'frequencies')
+    satellites = []
+    for satID, frequency in zip(satIDs, frequencies):
+        satellites.append(WeatherSatellite(satID, frequency))
+    
+    updateTLE(satellites, tlePath)
+
+    # loop until it's time to capture data
+    while(True):
+        satQueue = sorted(satellites, key=lambda p : p.predictNextPass(qth).passTime )
+        nextSat = satQueue[0]
+        currentTime = datetime.datetime.now(datetime.timezone.utc)
+        timeUntilPass = (satQueue[0].nextPass.passTime - currentTime).seconds
+        
+        if(timeUntilPass>0):
+            logging.info('Waiting for {} at {} UTC'.format(nextSat.identifier, nextSat.nextPass.passTime))
+            time.sleep(timeUntilPass)
+        else:
+            logging.info('Beginning capture of {} at {}: duration {}, elevation {} degrees'.format(nextSat.identifier, nextSat.nextPass.passDuration,nextSat. nextPass.elevation ))
+            recordChunksFM(nextSat.frequency, nextSat.nextPass.duration, 60)
+           
+        time.sleep(10)
